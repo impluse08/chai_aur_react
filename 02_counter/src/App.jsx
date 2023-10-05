148e4efeabffd71e4a03416a5b06949457bd093f
Impluse08 <pranavsarwe007@gmail.com>
2023-10-05 07:40:29 +0530

    Why you need hooks

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -4,30 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [counter, setCounter] = useState(15)
+  // ye counter upar wala sare niche wale counters ko update karega and useState(15) is the current counter value
+  // let counter  = 15;
+  // document.querySelector('.add').addEventListener('click',function(){
+  //   counter = counter + 1
+  //   document.querySelector('.add').innerHTML = `Add Value ${counter}`
+  //   document.querySelector('.c1').innerHTML = `Counter Value: ${counter}`
+  //   document.querySelector('.rm').innerHTML = `Remove Value ${counter}`
+  //   document.querySelector('.foot').innerHTML = `This is ${counter}`
+  // })
+  const addValue = function () {
+    // counter = counter + 1
+    if (counter < 20){
+      setCounter(counter + 1) 
+    }
+    // console.log(`Clicket ${counter}`);
+  }
+  const rmValue = function(){
+    if (counter > 0){
+      setCounter(counter -1)
+    }
+    // console.log(`Clicket ${counter}`);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Chai aur React</h1>
+    <h2 class="c1">Counter Value: {counter}</h2>
+    <button class="add" onClick={addValue}>Add Value {counter}</button>
+    <br />
+    <button class="rm" onClick={rmValue}>Remove Value {counter}</button>
+    <footer class="foot">This is {counter}</footer>
     </>
   )
 }
