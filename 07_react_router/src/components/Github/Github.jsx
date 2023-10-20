92eeeefe67eab8afb12aa0d3fb61609da09c7a10
Impluse08 <pranavsarwe007@gmail.com>
2023-10-20 17:40:26 +0530

    react github api

--- 07_react_router/src/components/Github/Github.jsx
+++ 07_react_router/src/components/Github/Github.jsx
@@ -1,8 +1,19 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useState } from "react";
 function Github(){
+    const [data, setData] = useState([])
+    useEffect(() =>
+    {
+        fetch('https://api.github.com/users/hiteshchoudhary')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+    }
+    , [])
     return(
-        <div className="bg-gray-700 text-white">Github</div>
+        <div className="bg-gray-700 text-white">Github followers: {data.followers}</div>
     );
 }
 
