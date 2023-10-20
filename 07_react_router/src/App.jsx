4e139ffb007aebcd3fe69801b46e1315a7c681da
Impluse08 <pranavsarwe007@gmail.com>
2023-10-20 14:39:47 +0530

    React Router

--- 07_react_router/src/App.jsx
+++ 07_react_router/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
@@ -8,9 +7,6 @@ function App() {
 
   return (
     <>
-    <Header />
-    <Home />
-    <Footer />
     </>
     )
 }
---
+++ 07_react_router/src/components/Github/Github.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Github(){
+    return(
+        <div className="bg-gray-700 text-white">Github</div>
+    );
+}
+
+export default Github
